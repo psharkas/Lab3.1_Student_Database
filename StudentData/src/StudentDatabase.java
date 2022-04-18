fc5d45bedd2be023ea63b0fb43484bae42ee27fc
psharkas <petersharkas7@gmail.com>
2022-04-17 21:11:10 -0400

    Second commit, added loops and valid student selection

--- StudentData/src/StudentDatabase.java
+++ StudentData/src/StudentDatabase.java
@@ -1,4 +1,5 @@
 import java.util.InputMismatchException;
+import java.lang.ArrayIndexOutOfBoundsException;
 import java.util.Scanner;
 
 public class StudentDatabase {
@@ -14,29 +15,57 @@ public class StudentDatabase {
 		
 		int inputNum = 0;
 		System.out.println("Welcome to our Java class. Which student would you like to learn more about? (enter a number 1-5): ");
-		try {
-			inputNum = scnr.nextInt();
-		} catch(InputMismatchException e) {
-			scnr.next();
-			System.out.println("You did not enter a number.");
-		} catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("You did not enter a number within 1-5.");
-		}
 		
-		System.out.println("Student " + inputNum + " is " + studentName[inputNum-1] + ". What would you like to know about " + studentName[inputNum-1] + "? (enter “hometown” or “age”): ");
-		// why do I need this extra scanner new line? stack overflow recommended this and it worked. Can't grab the response without it.
-		scnr.nextLine();
+		boolean cont1 = true;
+		boolean cont2 = true;
+		
+		do {
+			try {
+				inputNum = scnr.nextInt();
+			} catch(InputMismatchException e) {
+				scnr.next();
+			} 
+			
+			try {
+				System.out.println("Student " + inputNum + " is " + studentName[inputNum-1] + ". What would you like to know about " + studentName[inputNum-1] + "? (enter “hometown” or “age”): ");
+				scnr.nextLine();
+				cont1 = false;
+			}catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("You did not enter a number within 1-5. Please try again");
+			}
+		}while(cont1);
+
 		String response = scnr.nextLine();
 		
-		if (response.equalsIgnoreCase("age")) {
-			System.out.println(studentName[inputNum-1] + "is " + studentAge[inputNum-1] + " years old. Would you like to know more? (enter \"yes\" or \"no\"): ");
-		}
-		else if (response.equalsIgnoreCase("hometown")) {
-			System.out.println(studentName[inputNum-1] + " is from " + studentHometown[inputNum-1] + ". Would you like to know more? (enter \"yes\" or \"no\"): ");
-		}
-		
-		
+		do {
+			try {
+				if (response.equalsIgnoreCase("age")) {
+					System.out.println(studentName[inputNum-1] + "is " + studentAge[inputNum-1] + " years old. Would you like to know more? (enter \"yes\" or \"no\"): ");
+					String response2 = scnr.nextLine();
+					if (response2.equalsIgnoreCase("no")) {
+						cont2 = false;
+					} 
+				}
+				else if (response.equalsIgnoreCase("hometown")) {
+					System.out.println(studentName[inputNum-1] + " is from " + studentHometown[inputNum-1] + ". Would you like to know more? (enter \"yes\" or \"no\"): ");
+					String response2 = scnr.nextLine();
+					if (response2.equalsIgnoreCase("no")) {
+						cont2 = false;
+					}
 
+				}
+				else {
+					System.out.println("That data does not exist. Please Try again. (enter \"hometown\" or \"age\") ");
+				}
+			}
+			catch(InputMismatchException e){
+				scnr.next();
+				System.out.println("That data does not exist. Please Try again. (enter \"hometown\" or \"age\") ");
+			}
+		}while (cont2);
+		
+		
+	
 	}
 
 }
